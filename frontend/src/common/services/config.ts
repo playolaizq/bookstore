ade733529251ae2376bde9acc3c83cee1c6506aa
playolaizq <pol944li@gmail.com>
2023-07-18 17:00:47 +0200

    feat: vite server base url env variable

--- frontend/src/common/services/config.ts
+++ frontend/src/common/services/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3333/v1';
+const BASE_URL = `${import.meta.env.VITE_SERVER_BASE_URL}/v1`;
 
 export const instance = axios.create({
   baseURL: BASE_URL,
