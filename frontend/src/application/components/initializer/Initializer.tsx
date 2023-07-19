d11add8f352c8830de512ba6bdc19c15107e6e5d
playolaizq <pol944li@gmail.com>
2023-07-19 10:46:08 +0200

    feat: style initial loading container

--- frontend/src/application/components/initializer/Initializer.tsx
+++ frontend/src/application/components/initializer/Initializer.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { useUser } from '#/application/state/user';
 import { getLocalStorageItem } from '#/common/utils/local-storage';
 import { getUser } from '#/common/services/users';
+import classes from './Initializer.module.css';
 
 function Initializer() {
   const { setUser } = useUser();
@@ -13,13 +14,24 @@ function Initializer() {
     if (userId) {
       getUser(userId)
         .then((userData) => setUser(userData))
-        .finally(() => setInitialLoading(false));
+        .finally(() => {
+          setTimeout(() => {
+            setInitialLoading(false);
+          }, 500);
+        });
     } else {
       setInitialLoading(false);
     }
   }, []);
 
-  return initialLoading ? <div>Loading...</div> : <Outlet />;
+  return initialLoading ? (
+    <div className={classes.loadingContainer}>
+      <img className={classes.loadingImg} src="/logo.svg" alt="Bookstore" />
+      <h2>Loading Bookstore</h2>
+    </div>
+  ) : (
+    <Outlet />
+  );
 }
 
 export default Initializer;
